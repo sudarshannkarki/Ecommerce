273f12e792140f32ee12dc71423d42bd6bbd0cc2
Laptop <sudarshannkarki@gmail.com>
2020-08-22 11:06:41 +0300

    Task no. 3 Add two views and their urls.
    - Add functions
    - Create urls.py in estore app
    - Create templates for the html files
    - Register the urls in the project urls

---
+++ estore/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    # path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('', views.contact, name='contact'),
+]
