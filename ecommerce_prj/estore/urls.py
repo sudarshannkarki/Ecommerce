625a004933f4be15599bf0b891f61c224e06f40f
Laptop <sudarshannkarki@gmail.com>
2020-08-22 13:06:17 +0300

    Task no. 4 Add menu in base.html
    - Register url in base.html
    - Use template tagging in home.html

--- ecommerce_prj/estore/urls.py
+++ ecommerce_prj/estore/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('', views.contact, name='contact'),
+    path('contact/', views.contact, name='contact'),
 ]
